8fb7dee4398644a03fc0ee66be12f220755cc90c
KEERTHINAIK R <keerthinaik.gitlab.cicd@gmail.com>
2020-06-22 00:31:30 +0530

    added create_superuser for custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -7,9 +7,27 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and saves a new user"""
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
 
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(
+            email=email,
+            password=password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+
         user.save(using=self._db)
 
         return user
